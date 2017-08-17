de604b47dce7a8f742c9ae77dc1b7c2c6a8ec391
fgeraci-devops-zen <fgeraci@devops-zen.com>
2017-08-17 14:29:32 +0200

    final commit

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,18 +4,17 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
-    |> filter_odd_square
+    |> filter_odd_squares
     |> build_pixel_map
     |> draw_image
     |> save_image(input)
   end
 
   def save_image(image, input) do
-      File.write("#{input}.png", image)
-    
+    File.write("#{input}.png", image)
   end
 
-  def draw_image (%Identicon.Image{color: color, pixel_map: pixel_map }) do
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
     image = :egd.create(250, 250)
     fill = :egd.color(color)
 
@@ -24,15 +23,13 @@ defmodule Identicon do
     end
 
     :egd.render(image)
-
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
-    pixel_map = 
-      Enum.map grid, fn({_code, index}) ->
+    pixel_map = Enum.map grid, fn({_code, index}) ->
       horizontal = rem(index, 5) * 50
       vertical = div(index, 5) * 50
-      
+
       top_left = {horizontal, vertical}
       bottom_right = {horizontal + 50, vertical + 50}
 
@@ -40,28 +37,20 @@ defmodule Identicon do
     end
 
     %Identicon.Image{image | pixel_map: pixel_map}
-
   end
 
-  def filter_odd_square(%Identicon.Image{grid: grid} = image) do
-    Enum.filter grid, fn({code , _index}) ->
-    rem(code, 2) == 0
+  def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
     end
 
     %Identicon.Image{image | grid: grid}
-
-  end
-  # Get images and rgb values for colors
-  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-    %Identicon.Image{image | color: {r, g, b}}
   end
 
-  # Let's build the grid and define which squares has to be colored and which not.
   def build_grid(%Identicon.Image{hex: hex} = image) do
     grid =
       hex
       |> Enum.chunk(3)
-      # Passing reference to a function
       |> Enum.map(&mirror_row/1)
       |> List.flatten
       |> Enum.with_index
@@ -70,22 +59,19 @@ defmodule Identicon do
   end
 
   def mirror_row(row) do
-    # [145, 46, 200] for "adfs"
     [first, second | _tail] = row
 
-
-    # [145, 46, 200, 46, 145]
-
     row ++ [second, first]
-    
   end
 
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
-    
   end
 end
