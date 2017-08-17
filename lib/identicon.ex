28ac6d229da4c582e7e189e6d32d12c973c5fb79
fgeraci-devops-zen <fgeraci@devops-zen.com>
2017-08-17 12:12:36 +0200

    got rgb colors

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,7 +2,17 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
   end
+
+  # Get images and rgb values for colors
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  # Let's build the grid and define which squares has to be colored and which not.
+
+
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
